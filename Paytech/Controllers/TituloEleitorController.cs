b02ae4a3d8460d90fdfa42cdbde704d405d44085
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-09-15 13:05:08 -0300

    commit final (talvez) com melhorias nas utilidades da controller

--- Paytech/Controllers/TituloEleitorController.cs
+++ Paytech/Controllers/TituloEleitorController.cs
@@ -14,7 +14,10 @@ namespace Paytech.Controllers
         public ActionResult Insert([FromBody] TituloEleitor tituloEleitor)
         {
             if (Utilidades.ValidacaoTituloEleitor(tituloEleitor.Numero_Titulo) == false)
-                return StatusCode(413, "Este título não é válido, digite-o corretamente!");
+                return StatusCode(413, "Título inválido, digite-o corretamente!");
+
+            if (Utilidades.ValidacaoSecaoEZona(tituloEleitor.Secao, tituloEleitor.Zona) == false)
+                return StatusCode(413, "Seção e/ou zona inválidas, digite corretamente!");
 
             if (new TituloEleitorService().Insert(tituloEleitor))
                 return StatusCode(200, tituloEleitor);
@@ -47,6 +50,9 @@ namespace Paytech.Controllers
             if (!Utilidades.ValidacaoTituloEleitor(numeroTitulo)) 
                 return StatusCode(413, "Este título não é válido, digite-o corretamente!");
 
+            if (Utilidades.ValidacaoSecaoEZona(secao, zona) == false)
+                return StatusCode(413, "Seção e/ou zona inválidas, digite corretamente!");
+
             var titulo = new TituloEleitorService().GetByTitulo(numeroTitulo);
             if (titulo == null) return NotFound("Título de eleitor não encontrado");
             new TituloEleitorService().AlterarDados(numeroTitulo, secao, zona);
@@ -75,18 +81,16 @@ namespace Paytech.Controllers
     {
         public static bool ValidacaoTituloEleitor(string numeroTitulo)
         {
-            if (string.IsNullOrEmpty(numeroTitulo) || numeroTitulo.Length != 12)
-            {
-                return false;
-            }
-            foreach (char c in numeroTitulo)
-            {
-                if (!char.IsDigit(c))
-                {
-                    return false;
-                }
-            }
-            return true;
+            return !string.IsNullOrEmpty(numeroTitulo) &&
+                       numeroTitulo.Length == 12 &&
+                       numeroTitulo.All(char.IsDigit) &&
+                       !numeroTitulo.Any(char.IsLetter);
+        }
+
+        public static bool ValidacaoSecaoEZona(string secao, string zona)
+        {
+            return secao.Length <= 5 && zona.Length <= 5 &&
+                   secao.All(char.IsDigit) && zona.All(char.IsDigit);
         }
     }
 }
