b411b8d1a9485cf4d9514b692aabfd7023940eae
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-09-20 11:55:44 -0300

    entidade CNH finalizada, porém não foi testada! Aguardando o banco retomar funcionamento

---
+++ Paytech/Services/CnhService.cs
@@ -0,0 +1,39 @@
+﻿using Paytech.Models;
+using Paytech.Repositories;
+
+namespace Paytech.Services
+{
+    public class CnhService
+    {
+        private readonly ICnhRepository _cnhRepository;
+
+        public CnhService()
+        {
+            _cnhRepository = new CnhRepository();
+        }
+
+        public bool Insert(Cnh cnh)
+        {
+            return _cnhRepository.Insert(cnh);
+        }
+
+        public List<Cnh> GetAll()
+        {
+            return _cnhRepository.GetAll();
+        }
+        public Cnh GetByNumCnh(string num_cnh)
+        {
+            return _cnhRepository.GetByNumCnh(num_cnh);
+        }
+
+        public void AlterarCnh(string num_cnh, string cateforia, DateTime? dt_emissao, DateTime? dt_vencimento)
+        {
+            _cnhRepository.AlterarCnh(num_cnh, cateforia, dt_emissao, dt_vencimento);
+        }
+        public void Delete(string username)
+        {
+            _cnhRepository.Delete(username);
+        }
+
+    }
+}
