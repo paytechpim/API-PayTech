0f308bf94175bb5db00f27d568b7eebbe14c31e4
ddomingos.ext <ddomingos.ext@orizon.com.br>
2023-09-25 12:34:54 -0300

    Update string conection sql

--- Paytech/Repositories/CarteiraTrabalhoRepository.cs
+++ Paytech/Repositories/CarteiraTrabalhoRepository.cs
@@ -6,24 +6,28 @@ namespace Paytech.Repositories
 {
     public class CarteiraTrabalhoRepository : ICarteiraTrabalhoRepository
     {
-        private string _conn = "Server=paytech.database.windows.net;Database=Paytech;User Id=diego;Password=Paytech2023;";
+        private IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
         public bool Insert(CarteiraTrabalho carteiraTrabalho)
         {
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             db.Execute(CarteiraTrabalho.INSERT, carteiraTrabalho);
             return true;
         }
 
         public List<CarteiraTrabalho> GetAll()
         {
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             var carteirasTrabalho = db.Query<CarteiraTrabalho>(CarteiraTrabalho.SELECT_ALL).ToList();
             return carteirasTrabalho;
         }
 
         public CarteiraTrabalho GetById(string numCtps, string UF)
         {
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             var carteiraTrabalho = db.QuerySingleOrDefault<CarteiraTrabalho>(CarteiraTrabalho.SELECT_BY_ID, new { NumCtps = numCtps, UFCarteira = UF });
             return carteiraTrabalho;
         }
@@ -35,14 +39,14 @@ namespace Paytech.Repositories
             carteiraTrabalho.Orgao = orgao;
             carteiraTrabalho.Serie = serie;
             carteiraTrabalho.Cbo = cbo;
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             db.Execute(CarteiraTrabalho.UPDATE, carteiraTrabalho);
         }
 
         public void Delete(string numCtps, string UF)
         {
             var carteiraTrabalho = GetById(numCtps, UF);
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             db.Execute(CarteiraTrabalho.DELETE, carteiraTrabalho);
         }
     }
--- Paytech/Repositories/LoginRepository.cs
+++ Paytech/Repositories/LoginRepository.cs
@@ -6,27 +6,30 @@ namespace Paytech.Repositories
 {
     public class LoginRepository : ILoginRepository
     {
-        private string _conn = "Server=paytech.database.windows.net;Database=Paytech;User Id=diego;Password=Paytech2023;";
+        private IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
 
         public List<Login> GetAll()
         {
             List<Login> list = new();
 
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             var logins = db.Query<Login>(Login.SELECT_ALL);
             return (List<Login>)logins;
         }
 
         public Login GetByUsername(string Username)
         {
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             var login = db.QuerySingleOrDefault<Login>(Login.SELECT_BY_USERNAME, new { Nome_Usuario = Username });
             return login;
         }
 
         public bool Insert(Login login)
         {
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             db.Execute(Login.INSERT, login);
             return true;
         }
@@ -35,7 +38,7 @@ namespace Paytech.Repositories
         public void Delete(string username)
         {
             Login login = GetByUsername(username);
-            using var db = new SqlConnection(_conn);
+            using var db = new SqlConnection(configuration.GetConnectionString("sql"));
             db.Execute(Login.DELETE, login);
         }
     }
